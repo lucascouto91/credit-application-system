35f0ef791764806bed642bb4911e6f64c10f7494
Lucas Couto <lucascouto91@gmail.com>
2023-12-16 19:17:50 -0300

    fix: implemented Endpoints CustomerResource update class 22.

--- src/main/kotlin/me/dio/creditapplicationsystem/dto/CustomerUpdateDto.kt
+++ src/main/kotlin/me/dio/creditapplicationsystem/dto/CustomerUpdateDto.kt
@@ -11,10 +11,10 @@ data class CustomerUpdateDto(
     val street: String
 ) {
     fun toEntity(customer: Customer): Customer {
-        customer.firstName = this.firstName,
-        customer.lastName = this.lastName,
-        customer.income = this.income,
-        customer.address.street = this.street,
+        customer.firstName = this.firstName
+        customer.lastName = this.lastName
+        customer.income = this.income
+        customer.address.street = this.street
         customer.address.zipCode = this.zipCode
         return customer
     }
