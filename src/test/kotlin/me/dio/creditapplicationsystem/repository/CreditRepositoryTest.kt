fb9794bd0bfc805c60b9558823e8db28e24e9b71
Lucas Couto <lucascouto91@gmail.com>
2023-12-25 16:33:14 -0300

    test: Integration test, CreditRepositoryTest-findAllByCustomerId.

--- src/test/kotlin/me/dio/creditapplicationsystem/repository/CreditRepositoryTest.kt
+++ src/test/kotlin/me/dio/creditapplicationsystem/repository/CreditRepositoryTest.kt
@@ -51,6 +51,18 @@ class CreditRepositoryTest {
 
     }
 
+    @Test
+    fun `should find all credits by customer id`() {
+        //given
+        val customerId: Long = 1L
+        //when
+        val creditList: List<Credit> = creditRepository.findAllByCustomerId(customerId)
+        //then
+        Assertions.assertThat(creditList).isNotEmpty
+        Assertions.assertThat(creditList.size).isEqualTo(2)
+        Assertions.assertThat(creditList).contains(credit1, credit2)
+    }
+
 
     private fun buildCredit(
         creditValue: BigDecimal = BigDecimal.valueOf(100.0),
@@ -71,8 +83,7 @@ class CreditRepositoryTest {
         password: String = "12345",
         zipCode: String = "12345",
         street: String = "Rua da Cami",
-        income: BigDecimal = BigDecimal.valueOf(1000.0),
-        id: Long = 1L
+        income: BigDecimal = BigDecimal.valueOf(1000.0)
     ) = Customer(
         firstName = firstName,
         lastName = lastName,
