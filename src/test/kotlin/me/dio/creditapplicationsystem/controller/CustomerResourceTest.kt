3725c916d449afaa7fd580af5e7769fb52d45759
Lucas Couto <lucascouto91@gmail.com>
2023-12-26 20:43:37 -0300

    test: Integration test, CustomerResourceTest save-part3.

--- src/test/kotlin/me/dio/creditapplicationsystem/controller/CustomerResourceTest.kt
+++ src/test/kotlin/me/dio/creditapplicationsystem/controller/CustomerResourceTest.kt
@@ -94,6 +94,32 @@ class CustomerResourceTest {
             .andDo(MockMvcResultHandlers.print())
     }
 
+    @Test
+    fun `should not save a customer with firstName empty and return 400 status`() {
+        //given
+        val customerDto: CustomerDto = buildCustomerDto(firstName = "")
+        val valueAsString: String = objectMapper.writeValueAsString(customerDto)
+        //when
+        //then
+        mockMvc.perform(
+            MockMvcRequestBuilders.post(URL)
+                .content(valueAsString)
+                .contentType(MediaType.APPLICATION_JSON)
+        )
+            .andExpect(MockMvcResultMatchers.status().isBadRequest)
+            .andExpect(
+                MockMvcResultMatchers.jsonPath("$.title")
+                    .value("Bad Request! Consult the documentation ")
+            )
+            .andExpect(MockMvcResultMatchers.jsonPath("$.timestamp").exists())
+            .andExpect(MockMvcResultMatchers.jsonPath("$.status").value(400))
+            .andExpect(
+                MockMvcResultMatchers.jsonPath("$.exception")
+                    .value("class org.springframework.web.bind.MethodArgumentNotValidException")
+            )
+            .andExpect(MockMvcResultMatchers.jsonPath("$.details[*]").isNotEmpty)
+            .andDo(MockMvcResultHandlers.print())
+    }
 
     private fun buildCustomerDto(
         firstName: String = "Cami",
