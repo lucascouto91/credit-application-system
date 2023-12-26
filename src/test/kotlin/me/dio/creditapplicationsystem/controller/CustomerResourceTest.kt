b143e12318afe467165511ebf46daf7d1e06d839
Lucas Couto <lucascouto91@gmail.com>
2023-12-26 19:13:48 -0300

    test: Integration test, CustomerResourceTest save-part1.

--- src/test/kotlin/me/dio/creditapplicationsystem/controller/CustomerResourceTest.kt
+++ src/test/kotlin/me/dio/creditapplicationsystem/controller/CustomerResourceTest.kt
@@ -1,15 +1,22 @@
 package me.dio.creditapplicationsystem.controller
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import me.dio.creditapplicationsystem.dto.CustomerDto
 import me.dio.creditapplicationsystem.repository.CustomerRepository
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureWebMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+import java.math.BigDecimal
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -20,21 +27,63 @@ class CustomerResourceTest {
     private lateinit var customerRepository: CustomerRepository
 
     @Autowired
-    private lateinit var mockMvc: AutoConfigureWebMvc
+    private lateinit var mockMvc: MockMvc
 
     @Autowired
     private lateinit var objectMapper: ObjectMapper
 
     companion object {
-        const val URL: String = " /api/customers"
+        const val URL: String = "/api/customers"
     }
 
     @BeforeEach
     fun setup() = customerRepository.deleteAll()
+
     @AfterEach
     fun tearDown() = customerRepository.deleteAll()
 
+    @Test
+    fun `shloud create a customer and return 201 status`() {
+        //given
+        val customerDto: CustomerDto = buildCustomerDto()
+        val valueAsString: String = objectMapper.writeValueAsString(customerDto)
+        //when
+        //then
+        mockMvc.perform(MockMvcRequestBuilders.post(URL)
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(valueAsString))
+            .andExpect(MockMvcResultMatchers.status().isCreated)
+            .andExpect(MockMvcResultMatchers.jsonPath("$.firstName").value("Cami"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.lastName").value("Cavalcante"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.cpf").value("28475934625"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.email").value("camila@email.com"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.zipCode").value("12345"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.street").value("Rua da Cami"))
+            .andDo(MockMvcResultHandlers.print())
 
+    }
+
+
+    private fun buildCustomerDto(
+        firstName: String = "Cami",
+        lastName: String = "Cavalcante",
+        cpf: String = "28475934625",
+        income: BigDecimal = BigDecimal.valueOf(1000.0),
+        email: String = "camila@email.com",
+        password: String = "12345",
+        zipCode: String = "12345",
+        street: String = "Rua da Cami"
+    ) = CustomerDto(
+        firstName = firstName,
+        lastName = lastName,
+        cpf = cpf,
+        email = email,
+        income = income,
+        password = password,
+        zipCode = zipCode,
+        street = street
+
+    )
 
 
 }
